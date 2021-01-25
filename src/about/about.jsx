db7790475e8edbc312ca2f20f680a3e72d6dc37a
Mauro Nunes <maurao.dev@gmail.com>
2021-01-24 23:19:54 -0300

    Component page header

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import PageHeader from "../template/pageHeader";
 
 export default (props) => (
   <div>
-    <h1>Sobre</h1>
+    <PageHeader name="Sobre" small="Nós" />
+
+    <h2>Nossa história</h2>
+
+    <p>Lorem ipsum</p>
+
+    <h2>Missão e visão</h2>
+
+    <p>Lorem ipsum</p>
+
+    <h2>Impresa</h2>
+
+    <p>Lorem ipsum</p>
   </div>
 );
