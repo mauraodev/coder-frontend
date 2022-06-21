fde08d3944683399c1b6fa81b0554c75198008ec
Mauro Nunes <maurao.dev@gmail.com>
2022-06-20 21:36:14 -0300

    feat: todoForm com redux

--- src/todo/todoForm.jsx
+++ src/todo/todoForm.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import { connect } from "react-redux";
+
 import Grid from "../template/grid";
 import IconButton from "../template/iconButton";
 
-export default (props) => {
+const TodoForm = (props) => {
   const keyHandler = (e) => {
     if (e.key === "Enter") {
       e.shiftKey ? props.handleSearch() : props.handleAdd();
@@ -33,3 +35,7 @@ export default (props) => {
     </div>
   );
 };
+
+const mapToStateToProps = state => ({ description: state.todo.description });
+
+export default connect(mapToStateToProps)(TodoForm);
