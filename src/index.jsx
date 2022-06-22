3484cb64522853b8fff085630b15653935b4d9af
Mauro Nunes <maurao.dev@gmail.com>
2022-06-21 23:26:21 -0300

    feat: redux

--- src/index.jsx
+++ src/index.jsx
@@ -6,6 +6,9 @@ import { createStore } from "redux";
 import App from "./main/app";
 import reducers from './main/reducers';
 
-const store = createStore(reducers)
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+
+const store = createStore(reducers, devTools)
+
 
 ReactDom.render(<Provider store={store}><App /></Provider>, document.getElementById('app'));
